a8a76595254a3b99eaa13c9792cc6810bc413f13
Admin_arlax <98280280+Arj-jar@users.noreply.github.com>
2024-07-20 09:42:59 +0530

    test

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('no errors found.....wert.');
+function add(num1, num2, canprint, phrase) {
+    if (printing) {
+        var result = num1 + num2;
+        console.log(phrase + result);
+    }
+}
+var number1 = 10;
+var number2 = 20;
+var printing = true;
+var phrase = "the result is : ";
+add(number1, number2, printing, phrase);
--- app.ts
+++ app.ts
@@ -1,1 +1,13 @@
-console.log('no errors found.....wert.')
+function add(num1: number, num2: number, canprint: boolean, phrase: string) {
+  if (printing) {
+    var result = num1 + num2;
+    console.log(phrase + result);
+  }
+}
+
+var number1 = 10;
+var number2 = 20;
+var printing = true;
+var phrase = "the result is : ";
+
+add(number1, number2, printing, phrase);
